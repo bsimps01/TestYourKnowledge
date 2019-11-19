183334be5346e6f270bacf33876bece213b57857
Ben Simpson <b.albertsimpson@gmail.com>
2019-11-18 20:52:31 -0800

    placed update to ReadMe.md and functionality to quiz handling responses

--- Quiz.py
+++ Quiz.py
@@ -6,6 +6,9 @@ def question_one():
     if response == "C":
         print("That's right!")
         return 1
+    elif response == "c":
+        print("That's right!")
+        return 1
     else: 
         print("Sorry, the correct answer is C) 1977")
         return 0
@@ -14,6 +17,9 @@ def question_two():
     response2 = input("What is the name of the spaceship that is piloted by Han Solo and Chewbacca? A) Star Destroyer B) Millenium Falcon C) Lightyear Cruiser D) X-Wing ")
 
     if response2 == "B":
+        print("You got it!")
+        return 1
+    elif response2 == "b":
         print("You got it!")
         return 1
     else:
@@ -26,7 +32,13 @@ def question_three():
     if response3 == "T":
         print("Sorry that was wrong.")
         return 0
+    elif response3 == "t":
+        print("Sorry that was wrong.")
+        return 0
     elif response3 == "F":
+        print("Correct!")
+        return 1
+    elif response3 == "f":
         print("Correct!")
         return 1
     else:
@@ -38,12 +50,15 @@ def question_four():
     if response4 == "D":
         print("That was right!")
         return 1
+    elif response4 == "d":
+        print("That was right!")
+        return 1
     else:
         print("Sorry that was not right.")
         return 0
 
 def question_five():
-    response5 = int(input("How many Star Wars films were created that featured Mark Hamil? Including Episode 9, Plese enter a number:"))
+    response5 = int(input("How many Star Wars films were created that featured Mark Hamil? Including Episode 9, Plese enter a number: "))
 
     if response5 == 6:
         print("You got it!")
@@ -53,9 +68,12 @@ def question_five():
         return 0
 
 def question_six():
-    response6 = input("What kind of animal is Chewbacca? A) A Wookie B) An Ewok C) Sandperson D) Mandalorian")
+    response6 = input("What kind of animal is Chewbacca? A) A Wookie B) An Ewok C) Sandperson D) Mandalorian ")
 
     if response6 == "A":
+        print("That is correct!")
+        return 1
+    elif response6 == "a":
         print("That is correct!")
         return 1
     else:
@@ -63,9 +81,12 @@ def question_six():
         return 0
 
 def question_seven():
-    response7 = input("Who has the highest kill count in the Star Wars Universe? A) Darth Vader B) Luke Skywalker C) Princess Leia D) Emperor Palpatine E) Han Solo")
+    response7 = input("Who has the highest kill count in the Star Wars Universe? A) Darth Vader B) Luke Skywalker C) Princess Leia D) Emperor Palpatine E) Han Solo ")
     
     if response7 == "B":
+        print("That's right! The Death Star was a big one!")
+        return 1
+    if response7 == "b":
         print("That's right! The Death Star was a big one!")
         return 1
     else:
@@ -76,3 +97,40 @@ total_score = 0
 result = question_one() + question_two() + question_three() + question_four() + question_five() + question_six() + question_seven()
 total_score += result
 print(total_score)
+
+def final_score():
+    if total_score == 7:
+        print("Great job! You got them all right!")
+    elif total_score == 6:
+        print("Nice work, you got 6 right!")
+    elif total_score == 5:
+        print("You got 5 out of 7 right, not bad")
+    elif total_score == 4:
+        print("You got 4 out of 7, not bad.")
+    elif total_score == 3:
+        print("3 out of 7, better luck next time")
+    elif total_score == 2:
+        print("Ouch. 2 out of 7, not the greatest.")
+    elif total_score == 1:
+        print("Dang, the force is not strong with you. 1 out of 7")
+    else:
+        print("0 out of 7, do or do not, there is no try")
+
+def game_over():
+    game = input("Would you like to play again? Y to continue or N for no: ")
+
+    if game == "Y":
+        question_one()
+        question_two()
+        question_three()
+        question_four()
+        question_five()
+        question_six()
+        question_seven()
+        final_score()
+        game_over()
+    elif game == "N":
+        exit()
+
+final_score()
+game_over()
